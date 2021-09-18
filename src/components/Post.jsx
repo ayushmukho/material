c7275888de485f4c0ceda74272325986aef52ee7
ayushmukho <ayushmukho@gmail.com>
2021-09-18 22:34:37 +0530

    Material-UI

---
+++ src/components/Post.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Container, makeStyles, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  card:{
+    marginBottom: theme.spacing(5)
+  },
+  media:{
+    height: 200,
+    [theme.breakpoints.down('sm')]:{
+      height: 150,
+    }
+  }
+}));
+
+const Post = () => {
+
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia 
+          className={classes.media}
+          image='https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395__480.jpg'
+          title="My Pizza"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5">My Pizza Post</Typography>
+          <Typography variant="body2">
+            Lorem Ipsum is simply dummy text of the printing and 
+            typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+            ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+            make a type specimen book. It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged
+            Lorem Ipsum is simply dummy text of the printing and 
+            typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+            
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">Share</Button>
+        <Button size="small" color="primary">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Post;
